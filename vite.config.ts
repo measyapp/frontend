482d628e270cec9fb425bb679cc8fb8f677af38b
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-01-17 16:05:15 -0400

    Configuração vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig,loadEnv } from 'vite'
+import { fileURLToPath, URL } from "url";
 import react from '@vitejs/plugin-react'
 const env = loadEnv('', process.cwd(), '');
 const PORT = (env.VITE_PORT||3368) as number;
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-    {
-      find: './runtimeConfig',
-      replacement: './runtimeConfig.browser',
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  ]
-}
+  },
 })
