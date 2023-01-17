df27f1880c38b10b4116278d5ec0a86795b4ab7e
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-01-17 15:49:15 -0400

    Configuraçao de ambiente

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 const env = loadEnv('', process.cwd(), '');
 const PORT = (env.VITE_PORT||3368) as number;
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+    {
+      find: './runtimeConfig',
+      replacement: './runtimeConfig.browser',
+    },
+  ]
+}
 })
