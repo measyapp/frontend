204ab2a21c161131aba900ce0af433ec6d607875
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-01-18 15:09:32 -0400

    Alt .env

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 const url = process.env.API_URL;
 console.log(url)
+console.log(process.env.NODE_ENV);
 export const Api = axios.create({
     baseURL: process.env.API_URL,
     withCredentials: true,
