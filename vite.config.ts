f5c79b482dfd111c1189b0802dfdb869cc045441
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-01-18 15:31:56 -0400

    Config. host da api

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 const env = loadEnv('', process.cwd(), '');
 const PORT = (env.VITE_PORT||3368) as number;
+console.log('env: '+process.env.VITE_API_URL);
 export default defineConfig({
       plugins: [react()],
       server: {
