8601b51463b41bedcc8df73102f8646543a0db9f
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-01-18 15:02:47 -0400

    Altercação na utilização da api

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const url = process.env.API_URL;
 console.log(url)
 export const Api = axios.create({
-    baseURL: "http://localhost:3333",
+    baseURL: process.env.API_URL,
     withCredentials: true,
     headers: {'Access-Control-Allow-Origin': 'true'},
     
