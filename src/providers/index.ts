c05df89a25013aa8b60d8e3d2e5e7a8b675302ae
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-01-17 22:30:55 -0400

    Mudança no provider

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -3,7 +3,7 @@ import {loadEnv } from 'vite'
 const env = loadEnv('', process.cwd(), '');
 console.log(env.VITE_API_URL);
 export const Api = axios.create({
-    baseURL: env.VITE_API_URL,
+    baseURL: "https://measy-api-service.herokuapp.com",
     withCredentials: true,
     headers: {'Access-Control-Allow-Origin': 'true'},
     
