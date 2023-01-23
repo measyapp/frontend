647a849bae23f8763b70b8052e973763969cb7ed
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-01-23 17:58:44 -0400

    Alt. Header do Provider e Logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { BiHome } from "react-icons/bi";
 import { HiOutlineUserCircle,HiChevronLeft } from "react-icons/hi";
 import { ButtonIcon, CardButton } from "../components";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { MdCancel } from "react-icons/md";
 
@@ -12,6 +12,7 @@ export interface HeaderProps{
 }
 export function Header({children, clean=false}: HeaderProps){
     const {logout,isLogged} = useAuth();
+    const navegador = useNavigate();
     return (
         <div className='flex h-16 w-screen justify-between place-items-center bg-deep-blue px-10 text-bright-white items-center'>
             <div className="flex items-center" >
@@ -21,7 +22,7 @@ export function Header({children, clean=false}: HeaderProps){
                 <Link to="/"> <CardButton icon={<BiHome size={39}/>}/></Link>
             </div>
             {children}
-            {isLogged() ? <Link to="/"><CardButton label="Logout" icon={<MdCancel size={25} onClick={(e)=>logout()}/>}/> </Link> :<Link to="/login"> <CardButton icon={<HiOutlineUserCircle size={39} className=""/>}/></Link>}
+            {isLogged() ? <Link to="/"><CardButton label="Logout" icon={<MdCancel size={25} onClick={(e)=>{logout(); navegador("/");}}/>}/> </Link> :<Link to="/login"> <CardButton icon={<HiOutlineUserCircle size={39} className=""/>}/></Link>}
         </div>
         
     );
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 const url = process.env.API_URL;
-console.log(url)
-console.log(process.env.NODE_ENV);
-console.log(process.env.APP_URL);
+//console.log(url)
+//console.log(process.env.NODE_ENV);
+//console.log(process.env.APP_URL);
 export const Api = axios.create({
     baseURL: process.env.API_URL,
     withCredentials: true,
     headers: {
-        'Access-Control-Allow-Origin': 'true'
+        'Access-Control-Allow-Origin': 'true',
+        'Origin': process.env.API_URL
     },
 
     
