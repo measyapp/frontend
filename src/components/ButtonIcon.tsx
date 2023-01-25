f18ea67929e2f0a70ad421d3be3a4c216b980ded
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-01-25 12:20:04 -0400

    Ajuste na gravação de usuário

--- src/components/ButtonIcon.tsx
+++ src/components/ButtonIcon.tsx
@@ -10,7 +10,7 @@ export interface ButtonIconProps extends React.ButtonHTMLAttributes<HTMLButtonEl
 }
 export function ButtonIcon({icon,text, notActive=false,header=false, onClickAlt}: ButtonIconProps){
     return(
-        <button className={clsx(" flex gap-1 h-12  min-w-fit rounded-2xl px-3  text-bright-white text-center w-full ",{
+        <button type="button" className={clsx(" flex gap-1 h-12  min-w-fit rounded-2xl px-3  text-bright-white text-center w-full ",{
             'bg-deep-blue': notActive === false,
             'bg-inactive-blue': notActive==true,
             'ring-bright-white ring-2': header==true,
