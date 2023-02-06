87f15b1b07da410bff39c27f4687d76ccd099d21
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-02-06 15:46:55 -0400

    Criação de estruturas de avaliações

---
+++ src/services/RatingService.ts
@@ -0,0 +1,40 @@
+
+import {Api} from "../providers"
+import { IRatingData } from "../types"
+import { authToken } from "./AuthToken";
+   
+const getAll =  () => {
+    const token = authToken();
+    if (token!=='') Api.defaults.headers['x-access-token'] = token;
+    
+    return  Api.get<IRatingData[]>("/ratings");
+}
+const getById=  (id : any) => {
+    const token = authToken();
+    if (token!=='') Api.defaults.headers['x-access-token'] = token;
+    
+    return  Api.get<IRatingData>(`/ratings/${id}`);
+}
+const create = (data : IRatingData)=> {
+    const token = authToken();
+    if (token!=='') Api.defaults.headers['x-access-token'] = token; 
+    return Api.post<IRatingData>("/ratings",data);
+}
+const update = (id : number,data :IRatingData)=>{
+    const token = authToken();
+    if (token!=='') Api.defaults.headers['x-access-token'] = token; 
+    return Api.put(`/ratings/${id}`, data)
+}
+const remove = (id: number)=>{
+    const token = authToken();
+    if (token!=='') Api.defaults.headers['x-access-token'] = token;
+    return Api.delete(`/ratings/${id}`);
+}
+//const post = 
+export const RatingService = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove,
+}
---
+++ src/types/Rating.ts
@@ -0,0 +1,8 @@
+export interface IRatingData {
+    id:  number,
+    id_autor: number,
+    id_indicacao : number,
+    nota: number,
+    comentario: string;
+}
+
